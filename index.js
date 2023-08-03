5f0d2ea29c2844d1f31a0c14b80ec5866e191b40
tshark2907 <thiagovinicius2907@outlook.com>
2023-08-02 22:23:09 -0300

    Logica estruturada para adcionar produtos

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const data = {
     price: document.getElementById('price'),
     discount: document.getElementById('discount_%')
 };
-class product{
-    constructor(name, url, price, discount){
+const container = document.querySelector('.products_holder');
+
+class Product {
+    constructor(name, url, price, discount) {
         this.name = name;
         this.url = url;
         this.price = price;
@@ -16,21 +18,36 @@ class product{
 
 const products = [];
 
-let controle = 1;
+// Função para criar e adicionar os elementos do produto ao DOM
+function createProductElement(product) {
+    const holder = document.createElement('div');
+    holder.classList.add('product_frame');
 
-products.forEach(product){
-const holder = document.createElement('div');
-holder.classList.add('product_frame');
-const image = document.createElement('img');
-image.classList.add('product_image')
-if(product.)
+    const image = document.createElement('img');
+    image.classList.add('product_image');
+    image.src = product.url;
+
+    const product_name = document.createElement('span');
+    product_name.classList.add('product_title');
+    product_name.textContent = product.name;
+
+    const product_discount = document.createElement('span');
+    product_discount.classList.add('product_discount');
+    product_discount.textContent = product.discount;
+
+    holder.appendChild(image);
+    holder.appendChild(product_name);
+    holder.appendChild(product_discount);
+
+    container.appendChild(holder);
 }
 
-button.addEventListener("click", () =>{
-    if(data.name.value !== '' && data.url.value !== '' && data.price.value !== ''){
-        let produto = new product
-        (data.name.value,data.url.value,data.price,data.discount);
+button.addEventListener("click", () => {
+    if (data.name.value !== '' && data.url.value !== '' && data.price.value !== '') {
+        let produto = new Product(data.name.value, data.url.value, data.price.value, data.discount.value);
         products.push(produto);
-        console.log(produto)
+        console.log(produto);
+
+        createProductElement(produto); // Chama a função para criar e adicionar os elementos do produto ao DOM
     }
-})
+});
